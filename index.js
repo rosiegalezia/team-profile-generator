946d688bcef547373f922a30e368cf2acd1cd6ef
rosiegalezia <rosegalezia@gmail.com>
2024-01-22 10:54:13 +0000

    refactored, the initial set of questions works but then errors are thrown. Need to work out how to pass two sets of questions to inquirer

--- index.js
+++ index.js
@@ -16,111 +16,131 @@ const render = require("./src/page-template.js");
 
 function init() {
 
-    const teamInfo = [
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'managerName',
-                message: "Please enter the team manager's name",
-            },
-            {
-                type: 'input',
-                name: 'managerID',
-                message: "Please enter the team manager's ID number",
-            },
-            {
-                type: 'input',
-                name: 'managerEmail',
-                message: "Please enter the team manager's email address",
-            },
-            {
-                type: 'input',
-                name: 'managerOffice',
-                message: "Please enter the team manager's office number",
-            },
-            {
-                type: 'list',
-                name: 'addMember',
-                message: 'What would you like to do next?',
-                choices: ['Add an engineer', 'Add an intern', 'Finish building the team'],
-            },
-            // engineer option chosen
-            {
-                type: 'input',
-                name: 'engineerName',
-                message: "Please enter the engineer's name",
-                when: (answers) => answers['addMember'] === 'Add an engineer',
-            },
-            {
-                type: 'input',
-                name: 'engineerID',
-                message: "Please enter the engineer's ID number",
-                when: (answers) => answers['addMember'] === 'Add an engineer',
-            },
-            {
-                type: 'input',
-                name: 'engineerEmail',
-                message: "Please enter the engineer's email address",
-                when: (answers) => answers['addMember'] === 'Add an engineer',
-            },
-            {
-                type: 'input',
-                name: 'engineerGithub',
-                message: "Please enter the engineer's GitHub username",
-                when: (answers) => answers['addMember'] === 'Add an engineer',
-            },
-            // intern option chosen
-            {
-                type: 'input',
-                name: 'internName',
-                message: "Please enter the intern's name",
-                when: (answers) => answers['addMember'] === 'Add an intern',
-            },
-            {
-                type: 'input',
-                name: 'internID',
-                message: "Please enter the intern's ID number",
-                when: (answers) => answers['addMember'] === 'Add an intern',
-            },
-            {
-                type: 'input',
-                name: 'internEmail',
-                message: "Please enter the intern's email address",
-                when: (answers) => answers['addMember'] === 'Add an intern',
-            },
-            {
-                type: 'input',
-                name: 'internSchool',
-                message: "Please enter the intern's school",
-                when: (answers) => answers['addMember'] === 'Add an intern',
-            },
-            // work out what to do if 'finish team' is selected
-            // either an inquirer 'when' or an if statement
-        ])
-
-            // Call the render function and pass in an array containing all employee objects;
-            // The render function will generate and return a block of HTML including templated divs for each employee!
-
-            // TODO: ASk Benicio how to pass in array, forEach???
-
-            .then((answers) => {
-                
-                const renderHTML = render(answers)
-
-                fs.writeFile('team.html', renderHTML, (err) =>
-                err ? console.error(err) : console.log('Success!')
-            );
-            })
-
-
-        // Create an HTML file using the HTML returned from the render function.
-        // Write it to a file named team.html in the output folder.
-        // You can use the provided variable outputPath to target this location.
-        // TODO: Ask Benicio, do I call this from within the class?
-        // TODO: How does OUTPIUT_DIR work??
+    // Define the default questions
+    const initialQuestionsArr = [
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "Please enter the team manager's name",
+        },
+        {
+            type: 'number',
+            name: 'managerID',
+            message: "Please enter the team manager's ID number",
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "Please enter the team manager's email address",
+        },
+        {
+            type: 'number',
+            name: 'managerOffice',
+            message: "Please enter the team manager's office number",
+        },
+        // {
+        //     type: 'list',
+        //     name: 'addMember',
+        //     message: 'What would you like to do next?',
+        //     choices: ['Add an engineer', 'Add an intern', 'Finish building the team'],
+        // },
     ]
 
+    // Define the questions asked if answers.addMember is equal to 'Add an engineer' or 'Add an intern'
+    const employeeQuestionsArr = [
+        {
+            type: 'input',
+            name: 'employeeName',
+            message: "Please enter the employee's name",
+        },
+        {
+            type: 'number',
+            name: 'employeeID',
+            message: "Please enter the employee's ID number",
+        },
+        {
+            type: 'input',
+            name: 'employeeEmail',
+            message: "Please enter the employee's email address",
+        },
+    ]
+
+    // Define the questions asked if answers.addMember is equal to 'Add an engineer'
+    const engineerQuestionsArr = [
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: "Please enter the engineer's GitHub username",
+            when: (answers) => answers['addMember'] === 'Add an engineer',
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What would you like to do next?',
+            choices: ['Add an engineer', 'Add an intern', 'Finish building the team'],
+        },
+    ]
+
+    // Define the questions asked if answers.addMember is equal to 'Add an intern'
+    const internQuestionsArr = [
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: "Please enter the intern's school",
+            when: (answers) => answers['addMember'] === 'Add an intern',
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What would you like to do next?',
+            choices: ['Add an engineer', 'Add an intern', 'Finish building the team'],
+        },
+    ]
+
+    // work out what to do if 'finish team' is selected
+    // either an inquirer 'when' or an if statement
+
+
+    // Call the render function and pass in an array containing all employee objects;
+    // The render function will generate and return a block of HTML including templated divs for each employee!
+
+    // TODO: Ask Benicio how to pass in array, forEach??
+
+    // Create an HTML file using the HTML returned from the render function.
+    // Write it to a file named team.html in the output folder.
+    // You can use the provided variable outputPath to target this location.
+    // TODO: Ask Benicio, do I call this from within the class?
+    // TODO: How does OUTPIUT_DIR work??
+
+
+    function initialQuestions() {
+        inquirer.prompt(initialQuestionsArr)
+            .then((answers) => {
+
+                const renderTeam = render(answers)
+
+                if (answers.addMember === 'Add an engineer') {
+                    inquirer.prompt(employeeQuestionsArr)
+                    inquirer.prompt(engineerQuestionsArr)
+                } else
+                    if (answers.addMember === 'Add an intern') {
+                        inquirer.prompt(employeeQuestionsArr)
+                        inquirer.prompt(internQuestionsArr)
+                    } else {
+                        fs.writeFile('team.html', renderTeam, (err) =>
+                            err ? console.error(err) : console.log('Success!')
+                        );
+                    }
+            })
+    }
+
+    initialQuestions()
+
 }
+
+
+
+
 
 // call the function to initialise program
 init()
